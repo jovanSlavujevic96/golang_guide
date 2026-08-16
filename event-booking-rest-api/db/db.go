2d2921e19e9c705afedee0b387d218babbd6504e
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-08-16 11:59:20 +0000

    Add registrations table creation to database initialization

--- event-booking-rest-api/db/db.go
+++ event-booking-rest-api/db/db.go
@@ -50,4 +50,18 @@ func createTables() {
 	if err != nil {
 		panic("Could not create events table.")
 	}
+
+	const createdRegistrationsTable = `
+	CREATE TABLE IF NOT EXISTS registrations (
+	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	  user_id INTEGER,
+	  event_id INTEGER,
+	  FOREIGN KEY (user_id) REFERENCES users(id),
+	  FOREIGN KEY (event_id) REFERENCES events(id)
+	)
+	`
+	_, err = DB.Exec(createdRegistrationsTable)
+	if err != nil {
+		panic("Could not create registrations table.")
+	}
 }
